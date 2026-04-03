12dea4be8ec93dfe5e4c7b28de731fcf827b9e83
Kaguyo <gabrieltroop27@gmail.com>
2026-04-02 23:25:42 -0300

    feature: add moveset for guts

--- DelegateVoiceLines/DelegateVoiceLines/Core/Characters/MovesetFactory/FactoryMethods/GutsMoveset.cs
+++ DelegateVoiceLines/DelegateVoiceLines/Core/Characters/MovesetFactory/FactoryMethods/GutsMoveset.cs
@@ -1,7 +1,6 @@
 ﻿using DelegateTurnGame.Core.Characters.MovesetFactory.Interface;
-using DelegateTurnGame.Core.Systems.Combat;
 using System;
-using System.Xml.Linq;
+using System.Collections.Generic;
 
 namespace DelegateTurnGame.Core.Characters.MovesetFactory.FactoryMethods
 {
@@ -9,19 +8,72 @@ namespace DelegateTurnGame.Core.Characters.MovesetFactory.FactoryMethods
     {
         public static Moveset Create()
         {
-            Move basicAtk = Move.Create(
+            // 1. Ataque Básico (Dragon Slayer)
+            Move dragonSlayer = Move.Create(
                 "Dragon Slayer",
-                "Swings the sword blablalbal",
-                 new List<Hit> 
-                 {
-                     new Hit { Multiplier = 120.0f }
-                 },
-                 spCost: 0,
+                "Um balanço massivo que esmaga o oponente.",
+                new Hit[] { new Hit { Multiplier = 1.5f } },
+                spCost: 0,
+                selfModifyingFunc: (self) => self,
+                targetModifyingFunc: (self, target) => {
+                    Console.WriteLine($"{target.Name} foi impactado pelo peso da lâmina!");
+                }
+            );
 
+            // 2. Habilidade de Buff (Berserker Rage)
+            Move berserkerRage = Move.Create(
+                "Berserker Rage",
+                "Consome HP para aumentar drasticamente o ataque.",
+                new Hit[] { },
+                spCost: 10,
+                selfModifyingFunc: (self) => {
+                    float atk = self.Atk;
+                    atk *= 1.5f;
+                    self.Atk = (int)atk;
+                    self.Hp -= (self.Hp / 5);
+                    return self;
+                },
+                targetModifyingFunc: (self, target) => { }
+            );
 
-                 
-                 );
-            return new Moveset();
+            // 3. Multi-hit (Cannon Arm)
+            Move cannonArm = Move.Create(
+                "Cannon Arm",
+                "Um disparo explosivo seguido de um tiro de besta.",
+                new Hit[] {
+                    new Hit { Multiplier = 2.0f },
+                    new Hit { Multiplier = 0.5f }
+                },
+                spCost: 30,
+                selfModifyingFunc: (self) => self,
+                targetModifyingFunc: (self, target) => {
+                    Console.WriteLine("Explosão causada pelo canhão!");
+                }
+            );
+
+            // 4. Ultimate (Eclipse Slayer)
+            Move eclipseSlayer = Move.Create(
+                "Eclipse Slayer",
+                "Uma sequência frenética de golpes.",
+                new Hit[] {
+                    new Hit { Multiplier = 1.5f },
+                    new Hit { Multiplier = 1.5f },
+                    new Hit { Multiplier = 3.0f }
+                },
+                spCost: 100,
+                selfModifyingFunc: (self) => self,
+                targetModifyingFunc: (self, target) => {
+                    
+                }
+            );
+
+            // Retorna o Moveset contendo a lista de moves criados
+            return new Moveset(new List<Move> {
+                dragonSlayer,
+                berserkerRage,
+                cannonArm,
+                eclipseSlayer
+            });
         }
     }
 }
